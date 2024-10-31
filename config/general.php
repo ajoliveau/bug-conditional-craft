ec93a6a79d7dd7853f7b16185e707875c4a98da0
Arthur Joliveau-Breney <arthur@akufen.ca>
2024-10-31 13:51:52 -0400

    Init

---
+++ config/general.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ *
+ * @see \craft\config\GeneralConfig
+ */
+
+use craft\config\GeneralConfig;
+use craft\helpers\App;
+
+return [	
+	'*' => [
+		'autosaveDrafts' => false
+	]
+];
